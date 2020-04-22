c1c5fd46a08e9a0daad86064beb1fa42ee8e6ae2
Magda Aderibigbe <magdaaderibigbe@gmail.com>
2020-04-22 22:08:16 +0300

    Init project

---
+++ app.js
@@ -0,0 +1,27 @@
+function checkCashRegister(price, cash, cid) {
+    var output = { status: "", change: [] };
+    var change = parseFloat(cash - price).toFixed(2);
+
+
+    const due = {
+        "PENNY": 0.01,
+        "NICKEL": 0.05,
+        "DIME": 0.10,
+        "QUARTER": 0.25,
+        "ONE": 1.00,
+        "FIVE": 5.00,
+        "TEN": 10.00,
+        "TWENTY": 20.00,
+        "ONE HUNDRED": 100.00
+    };
+    if (change <= 0) {
+        output.status = "INSUFFICIENT_FUNDS"
+        return output
+    }
+
+
+
+    return change;
+}
+
+console.log(checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]));
